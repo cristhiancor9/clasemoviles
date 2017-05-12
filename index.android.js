a6d67a7bb76f9acaf18484f0747744a303fa222d
cristhiancor9 <cristhiancor9@gmail.com>
2017-05-12 09:30:04 -0500

    vistas y textos

---
+++ index.android.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import {styles} from './style';
+
+export default class clase1 extends Component {
+
+
+
+
+  render() {
+    return (
+      </View>
+        <View style={styles.contenedor}>
+         <Text>Hola Mundo</Text>
+     </View>
+    );
+  }
+}
+
+
+
+AppRegistry.registerComponent('clase1', () => clase1);
